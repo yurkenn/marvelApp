3e3e38a267024d97649a9828e6274afe6f9a573c
oguz yurken <oguz.yurken@gmail.com>
2022-11-17 02:29:44 +0300

    Add user reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import {configureStore} from '@reduxjs/toolkit';
 import favoriteReducer from './favoriteSlice';
+import userReducer from './userSlice';
 
 export default configureStore({
   reducer: {
     favorite: favoriteReducer,
+    user: userReducer,
   },
 });
