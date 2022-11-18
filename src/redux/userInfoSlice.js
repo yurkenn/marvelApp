47e5e8bee473c74f03c4cabcb073e45fc171535d
oguz yurken <oguz.yurken@gmail.com>
2022-11-18 03:46:34 +0300

    Create userinfoslicer

---
+++ src/redux/userInfoSlice.js
@@ -0,0 +1,25 @@
+import {createSlice} from '@reduxjs/toolkit';
+import auth from '@react-native-firebase/auth';
+export const userInfoSlice = createSlice({
+  name: 'userInfo',
+  initialState: {
+    displayName: auth().currentUser.displayName,
+    email: auth().currentUser.email,
+    photoURL: auth().currentUser.photoURL,
+  },
+  reducers: {
+    updateUserInfo: (state, action) => {
+      state.displayName = action.payload.displayName;
+      state.email = action.payload.email;
+      state.emailVerified = action.payload.emailVerified;
+      state.phoneNumber = action.payload.phoneNumber;
+      state.photoURL = action.payload.photoURL;
+    },
+  },
+});
+
+export const {updateUserInfo} = userInfoSlice.actions;
+
+export const selectUserInfo = state => state.userInfo;
+
+export default userInfoSlice.reducer;
