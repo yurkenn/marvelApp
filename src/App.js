933bce10d87fe88dcaf9481de170aa7deec2f93a
oguz yurken <oguz.yurken@gmail.com>
2022-11-22 01:22:18 +0300

    Add flash message component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {Provider} from 'react-redux';
 import Router from './navigation/Router';
 import store from './redux/store';
-
+import FlashMessage from 'react-native-flash-message';
 const App = () => {
   return (
     <Provider store={store}>
       <GestureHandlerRootView style={{flex: 1}}>
         <Router />
       </GestureHandlerRootView>
+      <FlashMessage position="top" />
     </Provider>
   );
 };
